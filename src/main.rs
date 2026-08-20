3214c86e03b394025e1dafcfe3a59480f25a6db5
OkRespire <148709418+OkRespire@users.noreply.github.com.>
2026-08-20 14:42:53 +0100

    finished the initial CLI with the proper output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-use std::{io, path::PathBuf, process::Command};
+use std::{fmt::Display, io, path::PathBuf, process::Command};
 
 use strip_ansi_escapes::strip_str;
+
+mod model;
+mod nix;
+
+enum PackageChangeType {
+    Kernel,
+    Updated,
+    Removed,
+    Added,
+    Rebuilt,
+    Other,
+}
 
 #[derive(Debug)]
 struct Generation {
@@ -15,15 +27,73 @@ struct Generation {
 #[derive(Debug)]
 struct PackageChange {
     pkg_name: String,
-    old_ver: Option<String>,
-    new_ver: Option<String>,
+    old_ver: Vec<String>,
+    new_ver: Vec<String>,
     size_delta: Option<Size>,
+}
+
+impl Display for PackageChange {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.pkg_name)?;
+
+        if !self.old_ver.is_empty() {
+            write!(f, ": ")?;
+            for (i, ver) in self.old_ver.iter().enumerate() {
+                if i > 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{ver}")?;
+            }
+        }
+
+        if !self.new_ver.is_empty() {
+            if self.old_ver.is_empty() {
+                write!(f, ": ")?;
+            } else {
+                write!(f, " -> ")?;
+            }
+            for (i, ver) in self.new_ver.iter().enumerate() {
+                if i > 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{ver}")?;
+            }
+        }
+
+        if self.old_ver.is_empty() {
+            write!(f, ": ")?;
+        }
+        if let Some(sd) = &self.size_delta {
+            if sd.delta != 0.0 {
+                if self.old_ver.is_empty() && self.new_ver.is_empty() {
+                    write!(f, "{}", sd)?;
+                } else {
+                    write!(f, ", {}", sd)?;
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
 struct Size {
     pub delta: f64,
     pub unit: String,
+}
+
+impl Display for Size {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", self.delta, self.unit)
+    }
+}
+impl Default for Size {
+    fn default() -> Self {
+        Size {
+            delta: 0.0,
+            unit: "".to_string(),
+        }
+    }
 }
 
 fn main() {
@@ -66,9 +136,9 @@ fn main() {
         panic!("Not found")
     };
     let (new_store_path, old_store_path) = if old_num < new_num {
-        (old_store_path, new_store_path)
-    } else {
         (new_store_path, old_store_path)
+    } else {
+        (old_store_path, new_store_path)
     };
 
     let output = Command::new("nix")
@@ -84,28 +154,51 @@ fn main() {
         .lines()
         .map(|line| parse_as_packages(line))
         .collect::<Vec<PackageChange>>();
-    println!("{:#?}", &pkgs);
+    eprintln!("{:#?}", &pkgs);
 
-    let mut size_diff: f64 = 0.0;
+    let mut kernel = Vec::new();
+    let mut updated = Vec::new();
+    let mut removed = Vec::new();
+    let mut added = Vec::new();
+    let mut rebuild = Vec::new();
+    let mut other = Vec::new();
 
-    for x in pkgs {
-        match x.size_delta {
-            Some(sd) => {
-                let mult = match sd.unit.as_str() {
-                    "KiB" => 1024.0,
-                    "MiB" => 1024.0_f64.powi(2),
-                    "GiB" => 1024.0_f64.powi(3),
-                    _ => 1.0,
-                };
-                size_diff += sd.delta * mult;
-            }
-            None => continue,
+    for p in pkgs {
+        match classify_package(&p) {
+            PackageChangeType::Kernel => kernel.push(p),
+            PackageChangeType::Updated => updated.push(p),
+            PackageChangeType::Removed => removed.push(p),
+            PackageChangeType::Added => added.push(p),
+            PackageChangeType::Rebuilt => rebuild.push(p),
+            PackageChangeType::Other => other.push(p),
         }
     }
 
-    // let bucket =
-
-    println!("{} Bytes", size_diff.round())
+    println!("--- Package Update Summary ---");
+    if !kernel.is_empty() {
+        println!("[Kernel Updates]");
+        print_vec(kernel);
+    }
+    if !updated.is_empty() {
+        println!("[Updated Packages]");
+        print_vec(updated);
+    }
+    if !removed.is_empty() {
+        println!("[Removed Packages]");
+        print_vec(removed);
+    }
+    if !added.is_empty() {
+        println!("[Added Packages]");
+        print_vec(added);
+    }
+    if !rebuild.is_empty() {
+        println!("[Rebuilt Packages]");
+        print_vec(rebuild);
+    }
+    if !other.is_empty() {
+        println!("[Other Changes]");
+        print_vec(other);
+    }
 }
 
 fn parse_as_gen(input: &str) -> Generation {
@@ -125,19 +218,14 @@ fn parse_as_gen(input: &str) -> Generation {
 
 fn parse_as_packages(input: &str) -> PackageChange {
     let input = strip_str(input);
-    let fields: Vec<&str> = input.split_whitespace().collect();
+    let fields: Vec<&str> = input
+        .split_whitespace()
+        .map(|s| s.trim_end_matches(","))
+        .collect();
+
     eprintln!("DEBUG fields: {:?}", fields);
     let name = fields[0].trim_end_matches(":").to_string();
-    let arrow_loc = fields.iter().position(|f| *f == "→");
-
-    /*/TODO:
-     * Change old_ver and new_ver into Vec<String>. Why? Sometimes there are more than
-     * one old/new versions of the package so using the location of arrow_loc we get the vector
-     * after name until the arrow_loc for the old, and for the new versions, it is after arrow_loc
-     * until the size. So in reality this will look like
-     * name [new_ver1, new_ver2, ... ,new_ver_x] → [new_ver1, new_ver2, ..., new_ver_x] [delta, unit]
-     */
-    let (old_ver, new_ver) = if arrow_loc.is_none() {
+    let Some(arrow_idx) = fields.iter().position(|f| *f == "→") else {
         let delta: f64 = fields[1].parse().unwrap();
         let size = Some(Size {
             delta,
@@ -145,43 +233,83 @@ fn parse_as_packages(input: &str) -> PackageChange {
         });
         return PackageChange {
             pkg_name: name,
-            old_ver: None,
-            new_ver: None,
+            old_ver: Vec::new(),
+            new_ver: Vec::new(),
             size_delta: size,
         };
-    } else {
-        let new_ver_loc = arrow_loc.unwrap() + 1;
-        let old_ver = if fields[1] == "∅" || fields[1] == "ε" {
-            None
-        } else {
-            Some(fields[1].trim_end_matches(",").to_string())
-        };
-        let new_ver = if fields[new_ver_loc].trim_end_matches(",") == "∅"
-            || fields[new_ver_loc].trim_end_matches(",") == "ε"
-        {
-            None
-        } else {
-            Some(fields[new_ver_loc].trim_end_matches(",").to_string())
-        };
-        (old_ver, new_ver)
     };
 
-    let size_delta: Option<Size> = if fields.len() >= 6 {
-        let n = fields.len();
-        let unit_loc = n - 1;
-        let delta: f64 = fields[unit_loc - 1].parse().unwrap();
-        Some(Size {
-            delta: delta,
-            unit: fields[unit_loc].to_string(),
-        })
-    } else {
-        None
-    };
+    let mut new_end = fields.len();
+    let mut size_delta = None;
+
+    if fields.len() >= arrow_idx + 3 {
+        let delta_idx = fields.len() - 2;
+        if let Ok(delta) = fields[delta_idx].parse::<f64>() {
+            size_delta = Some(Size {
+                delta,
+                unit: fields[delta_idx + 1].to_string(),
+            });
+        }
+        new_end = delta_idx
+    }
+
+    let mut old_ver: Vec<String> = fields[1..arrow_idx]
+        .iter()
+        .map(|s| s.trim_end_matches(',').to_string())
+        .collect();
+
+    let mut new_ver: Vec<String> = fields[arrow_idx + 1..new_end]
+        .iter()
+        .map(|s| s.trim_end_matches(',').to_string())
+        .collect();
+
+    if old_ver.len() == 1 && (old_ver[0] == "∅" || old_ver[0] == "ε") {
+        old_ver.clear();
+    }
+
+    if new_ver.len() == 1 && (new_ver[0] == "∅" || new_ver[0] == "ε") {
+        new_ver.clear();
+    }
 
     PackageChange {
         pkg_name: name,
-        old_ver: old_ver,
-        new_ver: new_ver,
-        size_delta: size_delta,
+        old_ver,
+        new_ver,
+        size_delta,
+    }
+}
+
+fn print_vec(vec: Vec<PackageChange>) {
+    for v in vec {
+        println!("{}", v)
+    }
+    print!("\n")
+}
+
+fn is_kernel_package(name: &str) -> bool {
+    matches!(
+        name,
+        "linux"
+            | "linux-zen"
+            | "linux-headers"
+            | "linux-headers-static"
+            | "initrd-linux-zen"
+            | "linux-firmware"
+    )
+}
+
+fn classify_package(p: &PackageChange) -> PackageChangeType {
+    if is_kernel_package(&p.pkg_name) {
+        PackageChangeType::Kernel
+    } else if !p.old_ver.is_empty() && !p.new_ver.is_empty() {
+        PackageChangeType::Updated
+    } else if !p.old_ver.is_empty() {
+        PackageChangeType::Removed
+    } else if !p.new_ver.is_empty() {
+        PackageChangeType::Added
+    } else if p.size_delta.is_some() {
+        PackageChangeType::Rebuilt
+    } else {
+        PackageChangeType::Other
     }
 }
